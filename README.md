67c8976396a3ac12e67f9c4366ac0a817e668a43
Terrence <eightys3v3n@gmail.com>
2018-12-04 14:39:15 -0700

    Created instructions to get it working

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # raspi_backup_server
+
+Will run automatic backups of a Windows server over LAN using a Linux server to host a network share.
+
+## Setup
+### Windows
+1. [Install OpenSSH on the Windows server](https://winscp.net/eng/docs/guide_windows_openssh_server#installing_sftp_ssh_server).
+2. Configure the SSH server to port 443 or change the windows_ssh_port in main.py.
+3. Change the Windows variables at the top of main.py accordingly.
+
+### Linux
+3. [Install Samba on the Linux server](https://wiki.archlinux.org/index.php/samba).
+4. Add ports 137-139 and 445 to your firewall rules so the Samba share can be accessed.
+4. Add a share on the Linux machine by putting the following in `/etc/samba/smb.conf`:
+`
+[share_name]
+	comment = Some description
+	browseable = yes
+	writable = yes
+	guest ok = no
+	path = /the_path/to_your_backup/storing/folder
+`
+4. Modify the Linux variables at the top of main.py accordingly.
+5. Put this script on the Linux machine and run it.
--- main.py
+++ main.py
@@ -18,23 +18,26 @@ from datetime import datetime
 """
 
 
-linux_ip = "192.168.1.38"
-linux_username = "anonymous"
-linux_password = "Abc123"
-linux_path = "backups"
-windows_ip = "192.168.1.200"
-windows_username = "Terrence"
-windows_password = getpass.getpass("Windows SSH Password: ")
-# windows_password = "Abc123"
-windows_drive = "K:"
-windows_folder = "C:\\Users\\Terrence\\Desktop\\test"
-ssh_session = None
+linux_ip = "192.168.1.38" # IP address of the Linux machine that will hold the backups and run this script
+linux_username = "anonymous" # User name of Samba share hosted on Linux machine
+linux_password = "Abc123" # Password of Samba share hosted on Linux machine
+linux_path = "backups" # The name of the share; the part in the square brackets in smb.conf
+windows_ip = "192.168.1.200" # The IP address of the Windows server to be backed up
+windows_ssh_port = 443 # Port of the SSH server on the Windows machine
+windows_username = "Terrence" # User name to login as on the Windows machine
+windows_password = getpass.getpass("Windows SSH Password: ") # Password to login to Windows machine with | This is the same as the user password
+# windows_password = "Abc123" # If you want to hard-code the password comment the above line and uncomment this one. VERY UNRECOMMENDED AND POOR SECURITY PRACTICE.
+windows_drive = "K:" # What drive letter to use when mounting the network share
+windows_folder = "C:\\Users\\Terrence\\Desktop\\test" # What folder to backup
 
 
+# These are derived from the above variables.
 share_path = "\"\\\\{}\\{}\"".format(linux_ip, linux_path)
-# folder_name = datetime.now().__str__().replace(':', '-').replace(' ', '_')
-folder_name = "test"
+folder_name = datetime.now().__str__().replace(':', '-').replace(' ', '_')
+# folder_name = "test"
 backup_dst = "{drive}\\{folder_name}".format(drive=windows_drive, folder_name=folder_name)
+
+ssh_session = None
 
 
 def StartSamba():
@@ -46,19 +49,27 @@ def StopSamba():
 
 
 def ConnectSSH():
+	""" Create a new SSH connection and attempt to login to the Windows server.
+	"""
 	global ssh_session
 	ssh_session = pxssh.pxssh()
-	ssh_session.login(windows_ip, windows_username, windows_password, port="443", original_prompt="[>$]", auto_prompt_reset=False)
+	ssh_session.login(windows_ip, windows_username, windows_password, port=windows_ssh_port, original_prompt="[>$]", auto_prompt_reset=False)
 	# ssh_session.sync_original_prompt(sync_multiplier=1)
 	ssh_session.setwinsize(20, 200)
 
 
 def DisconnectSSH():
+	""" Disconnect from the Windows server.
+	"""
 	if ssh_session is not None:
 		ssh_session.logout()
+	ssh_session = None
 
 
 def MapNetworkDrive():
+	""" Map the network share on the Windows machine using a command prompt command via the SSH connection.
+		Prints the output of the command if it doesn't contain "The command completed successfully".
+	"""
 	cmd = "net use {drive} {share} /USER:{user} {passw}".format(
 		drive=windows_drive, share=share_path, user=linux_username, passw=linux_password)
 
@@ -68,6 +79,9 @@ def MapNetworkDrive():
 		print("Map drive output:", ssh_session.before.decode())
 
 def UnmapNetworkDrive():
+	""" Unmap the mapped network share.
+		Prints the output of the command if it doesn't contain "The command completed successfully".
+	"""
 	cmd = "net use {} /delete".format(share_path)
 
 	ssh_session.sendline(cmd)
@@ -77,6 +91,8 @@ def UnmapNetworkDrive():
 
 
 def BackupFiles():
+	""" Use robocopy to mirror the windows_path to the share under the folder_name derived at the top.
+	"""
 	cmd = "robocopy \"{from_}\" \"{to}\" /ZB /MIR /COPY:DAT".format(from_=windows_folder, to=backup_dst)
 
 	ssh_session.sendline(cmd)
@@ -85,8 +101,17 @@ def BackupFiles():
 
 
 def DoBackup():
-	# print("Starting Samba")
-	# StartSamba()
+	""" Actually do one backup:
+			- Start Samba
+			- Connect over SSH to Windows
+			- Map network share
+			- Backup files
+			- Unmap network share
+			- Disconnect from SSH
+			- Stop Samba
+	"""
+	print("Starting Samba")
+	StartSamba()
 	print("Connecting SSH")
 	ConnectSSH()
 	print("Mapping drive")
@@ -95,10 +120,10 @@ def DoBackup():
 	BackupFiles()
 	print("Unmapping drive")
 	UnmapNetworkDrive()
-	# print("Disconnecting SSH")
-	# DisconnectSSH()
-	# print("Stopping Samba")
-	# StopSamba()
+	print("Disconnecting SSH")
+	DisconnectSSH()
+	print("Stopping Samba")
+	StopSamba()
 
 
 def main():
