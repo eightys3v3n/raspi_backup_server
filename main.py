161ab6fd8c24c0b21ff9e0b85abfa6f2d28d85cb
Terrence <eightys3v3n@gmail.com>
2018-12-08 12:12:43 -0700

    Commenting and variable renaming

--- main.py
+++ main.py
@@ -14,79 +14,74 @@ from datetime import datetime
 		Get error code
 """
 
-""" CURRENTLY
-	The drive will not map on the windows PC using MapNetworkDrive()
-"""
 
+win_ip = "192.168.1.200" # The IP address of the Windows server to be backed up
+win_user = "Terrence" # User name to login as on the Windows machine
+win_pass = getpass("Windows SSH password: ") # Password to login to Windows machine with | This is the same as the user password
+win_ssh_port = 443 # Port of the SSH server on the Windows machine
+
+win_drive = "K:" # What drive letter to use when mounting the network share
+win_folder = "C:\\Users\\Terrence\\Desktop\\test" # What folder to backup
 
 linux_ip = "192.168.1.38" # IP address of the Linux machine that will hold the backups and run this script
-linux_username = "anonymous" # User name of Samba share hosted on Linux machine
-linux_password = "Abc123" # Password of Samba share hosted on Linux machine
+linux_user = "anonymous" # User name of Samba share hosted on Linux machine
+linux_pass = "Abc123" # Password of Samba share hosted on Linux machine
 linux_path = "backups" # The name of the share; the part in the square brackets in smb.conf
-windows_ip = "192.168.1.200" # The IP address of the Windows server to be backed up
-windows_ssh_port = 443 # Port of the SSH server on the Windows machine
-windows_username = "Terrence" # User name to login as on the Windows machine
-windows_password = getpass.getpass("Windows SSH Password: ") # Password to login to Windows machine with | This is the same as the user password
-# windows_password = "Abc123" # If you want to hard-code the password comment the above line and uncomment this one. VERY UNRECOMMENDED AND POOR SECURITY PRACTICE.
-windows_drive = "K:" # What drive letter to use when mounting the network share
-windows_folder = "C:\\Users\\Terrence\\Desktop\\test" # What folder to backup
 
+prompt_regex = "[>$]" # This regex is used to detect when a command has completed on the server. It should match the prompt.
 
 # These are derived from the above variables.
-share_path = "\"\\\\{}\\{}\"".format(linux_ip, linux_path)
-folder_name = datetime.now().__str__().replace(':', '-').replace(' ', '_')
-# folder_name = "test"
-backup_dst = "{drive}\\{folder_name}".format(drive=windows_drive, folder_name=folder_name)
-
-ssh_session = None
+share_path = "\"\\\\{}\\{}\"".format(linux_ip, linux_path) # Share path turns into "\\192.168.1.38\backups"
+folder_name = datetime.now().__str__().replace(':', '-').replace(' ', '_')[0:-7] # Name the destination folder based on the date "YYYY-MM-dd_HH-mm-ss"
+backup_dst = "{drive}\\{folder_name}".format(drive=win_drive, folder_name=folder_name) # Destination path "K:\folder_name"
 
 
 def ConnectSSH():
 	""" Create a new SSH connection and attempt to login to the Windows server.
 	"""
-	global ssh_session
-	ssh_session = pxssh.pxssh()
-	ssh_session.login(windows_ip, windows_username, windows_password, port=windows_ssh_port, original_prompt="[>$]", auto_prompt_reset=False)
-	# ssh_session.sync_original_prompt(sync_multiplier=1)
-	ssh_session.setwinsize(20, 200)
+	ssh_sess = pxssh.pxssh()
+	ssh_sess.login(win_ip, win_username, win_password, port=win_ssh_port, original_prompt=prompt_regex, auto_prompt_reset=False)
+	ssh_sess.setwinsize(20, 200)
+	return ssh_sess
 
 
-def DisconnectSSH():
+def DisconnectSSH(ssh_sess):
 	""" Disconnect from the Windows server.
 	"""
-	if ssh_session is not None:
-		ssh_session.logout()
-	ssh_session = None
+	if ssh_sess is not None:
+		ssh_sess.logout()
 
 
-def MapNetworkDrive():
+def MapNetworkDrive(ssh_sess):
 	""" Map the network share on the Windows machine using a command prompt command via the SSH connection.
 		Prints the output of the command if it doesn't contain "The command completed successfully".
 	"""
 	cmd = "net use {drive} {share} /USER:{user} {passw}".format(
-		drive=windows_drive, share=share_path, user=linux_username, passw=linux_password)
+		drive=win_drive, share=share_path, user=linux_user, passw=linux_pass)
+	# net use K: "\\192.168.1.38\\backups" /USER:Terrence linux_pass
 
-	ssh_session.sendline(cmd)
-	ssh_session.prompt()
-	if b"The command completed successfully" not in ssh_session.before:
-		print("Map drive output:", ssh_session.before.decode())
+	ssh_sess.sendline(cmd)
+	ssh_sess.prompt()
+	if b"The command completed successfully" not in ssh_sess.before:
+		print("Failed to map drive?\nMap drive output:", ssh_sess.before.decode())
 
-def UnmapNetworkDrive():
+def UnmapNetworkDrive(ssh_sess):
 	""" Unmap the mapped network share.
 		Prints the output of the command if it doesn't contain "The command completed successfully".
 	"""
-	cmd = "net use {} /delete".format(share_path)
+	cmd = "net use {} /DELETE".format(share_path)
+	# net use \\192.168.1.38\\backups /DELETE
 
-	ssh_session.sendline(cmd)
-	ssh_session.prompt()
-	if b"The command completed successfully" not in ssh_session.before:
-		print("Unmap drive output failed:", ssh_session.before.decode())
+	ssh_sess.sendline(cmd)
+	ssh_sess.prompt()
+	if b"The command completed successfully" not in ssh_sess.before:
+		print("Unmap drive output failed:", ssh_sess.before.decode())
 
 
 def BackupFiles():
-	""" Use robocopy to mirror the windows_path to the share under the folder_name derived at the top.
+	""" Use robocopy to mirror the win_path to the share under the folder_name derived at the top.
 	"""
-	cmd = "robocopy \"{from_}\" \"{to}\" /ZB /MIR /COPY:DAT".format(from_=windows_folder, to=backup_dst)
+	cmd = "robocopy \"{from_}\" \"{to}\" /ZB /MIR /COPY:DAT".format(from_=win_folder, to=backup_dst)
 
 	ssh_session.sendline(cmd)
 	ssh_session.prompt()
@@ -106,7 +101,7 @@ def DoBackup():
 	print("Starting Samba")
 	samba.Start()
 	print("Connecting SSH")
-	ConnectSSH()
+	ssh_sess = ConnectSSH()
 	print("Mapping drive")
 	MapNetworkDrive()
 	print("Backing up files")
@@ -114,7 +109,7 @@ def DoBackup():
 	print("Unmapping drive")
 	UnmapNetworkDrive()
 	print("Disconnecting SSH")
-	DisconnectSSH()
+	DisconnectSSH(ssh_sess)
 	print("Stopping Samba")
 	samba.Stop()
 
